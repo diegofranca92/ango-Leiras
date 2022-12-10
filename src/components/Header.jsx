5af4f2d5d8039f2560cae170a7ff03c7feb933fc
Ray Alcides <railayli2@hotmail.com>
2022-12-10 18:43:11 -0300

    header finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,17 @@ import styles from '../styles/components/header.module.css'
 export function Header() {
     return(
         <header className={styles.headerConteiner}>
-          <div className={styles.inforPrincipal}>
+          <div className={styles.infoBanner}>
              <h1>Angoleiras</h1>
-             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus et voluptatum deserunt delectus rem voluptas, eos tempora, molestiae deleniti quaerat dicta volupta</p>
+             <p>Lorem ipsum dolor sit amet consectetur fhtjy cbfjt dgrhtjyhfhd fhtj6j6th rfrhg5ry5ereg sgrjhthr greh db cbgfjyt adipisicing elit. Accusamus et voluptatum deserunt delectus rem voluptas, eos tempora, molestiae deleniti quaerat dicta volupta</p>
             <form>
-                <label htmlFor="">Onde está procurando o grupo ?</label>
+                <p> <strong> Onde está procurando o grupo ? </strong> </p>
               <select name="" id="">
                 <option value="">Selecione a localidade</option>
               </select>
             </form>
           </div>
-          <div className={styles.imgPrincipal}>
+          <div className={styles.imgBanner}>
             <img src="src\assets\mulher-capoeira.png" alt="desenho de uma mulher preta, obra do artista Patreon" />
           </div>
         </header>
