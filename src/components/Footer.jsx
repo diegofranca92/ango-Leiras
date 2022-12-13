7b9c9e3816e4b3f2e0dfef320bbd23482969d9ca
Ray Alcides <railayli2@hotmail.com>
2022-12-13 17:45:49 -0300

    footer finalizado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,17 @@ export function Footer() {
   return(
     <div className={styles.footerEstilo}>
       <p>Desenvolvido por 
-        <strong>Raiani Alcides</strong>
+        <strong> Raiani Alcides</strong>
       </p>
       <div className={styles.footerImg}>
+
+       <a href="https://github.com/rayalcides" target='_blank'>
         <img src={githubImg} alt="" />
+       </a>
+       <a href="https://www.linkedin.com/in/raiani-alcides-744984176/" target='_blank'>
         <img src={linkedinImg} alt="" />
+       </a>
+
       </div>
     </div>
   )
