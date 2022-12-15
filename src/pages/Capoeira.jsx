890567b3f41c44d50e594ed8ddd0316ec8a47619
Ray Alcides <railayli2@hotmail.com>
2022-12-15 19:36:38 -0300

    css da pagina +Capoeira e ajustes de responsividade

--- src/pages/Capoeira.jsx
+++ src/pages/Capoeira.jsx
@@ -1,11 +1,15 @@
+import styles from '../styles/pages/capoeira.module.css'      
+import img from '../assets/angola8.png' 
+import foto from '../assets/mulher-capoeira.png'                                                       
+
 export function Capoeira() {
     return(
       <main>
-        <h2>História da capoeira</h2>
-        <section>
+        <h1>História da capoeira</h1>
+        <section className={styles.containerCapoeira}>
             <p>A capoeira é uma expressão cultural brasileira que compreende os elementos: arte-marcial, esporte, cultura popular, dança e música. Foi criada no século XVII pelo povo escravizado da etnia banto e se difundiu por todo o Brasil. Hoje é considerada um dos maiores símbolos da cultura brasileira. <br />A palavra capoeira significa "o que foi mata", por meio da conexão dos termos ka'a ("mata") e pûer ("que foi"). Alude às áreas de mata rasa do interior do Brasil, onde era feita a agricultura indígena.
                Tem origem com os fugitivos da escravidão, os quais utilizavam frequentemente a vegetação rasteira para fugirem do encalço dos capitães-do-mato. Esses foram os primeiros capoeiristas. <br />A capoeira foi uma prática proibida no Brasil até 1930, quando passa a ser reconhecida como um símbolo da identidade brasileira. Em 2014, a Roda de Capoeira foi declarada Patrimônio Cultural Imaterial da Humanidade pela Unesco. </p>
-               <div>
+               <div className={styles.caractsCapoeira}>
                  <h3>Características da Capoeira</h3>
                  <ul>
                     <li>Acompanhamento de música: berimbau, e demais instrumentos, canto e palmas.</li>
@@ -13,16 +17,18 @@ export function Capoeira() {
                     <li>Graduação do capoeirista feita por cordas de cores diferentes atadas na cintura.</li>
                  </ul>
                </div>
-            <section>
-              <div>
+            <section className={styles.diferentesCapoeira}>
+              <div className={styles.descricaoAngola}>
                  <h3>Capoeira Angola</h3>
                  <p>A Angola é o estilo original que era praticado pelo povo escravizado. Essa maneira de jogar capoeira é caracterizada por ser mais lenta, composta de movimentos furtivos e executados de modo rasteiro.
                     O componente básico desse estilo é a malícia. Essa 'malandragem' consiste em simular movimentos que sirvam para enganar o oponente em combate.</p>
+                    <img src={img} alt="" />
               </div>
-              <div>
+              <div className={styles.descricaoRegional}>
                  <h3>Capoeira Regional</h3>
                  <p>A capoeira Regional é o estilo contemporâneo de capoeira. Ela possui atributos de outras artes-marciais em sua prática.
                     Esse estilo foi criado pelo Mestre Bimba e difundiu-se rapidamente pelo mundo, aumentando o número dos adeptos da capoeira em vários países.</p>
+                    <img src={foto} alt="" />
               </div>
             </section>
             <div>
